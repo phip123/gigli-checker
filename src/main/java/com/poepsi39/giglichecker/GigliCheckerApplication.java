ad76576900901a4a1bd0ad5612f857c050237e47
phip123 <poepsi39@gmail.com>
2018-04-19 13:08:01 +0200

    first commit

---
+++ src/main/java/com/poepsi39/giglichecker/GigliCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.poepsi39.giglichecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigliCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigliCheckerApplication.class, args);
+	}
+}
